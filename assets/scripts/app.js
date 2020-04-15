c8a4099eae61f69a2ee065709fc0a2097f10fc19
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-15 18:29:43 +0100

    fixed calcDescription and improved add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-   const result = num1 + num2;
-   return result;
+function add() {
+    const calcDescription = `${currentResult} + ${userInput.value}`;
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, calcDescription);
 }
 
-currentResult = add(1, 2);
-
-let calculationDescription = `(${defaultResult} + ${currentResult})`
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
 
