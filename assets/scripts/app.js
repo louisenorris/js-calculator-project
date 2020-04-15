4640d9698df7279cdaf475586f3633b362b3a7cc
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-15 12:27:45 +0100

    added add function to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,14 @@
-alert('this works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+   const result = num1 + num2;
+   return result;
+}
+
+currentResult = add(1, 2);
+
+let calculationDescription = `(${defaultResult} + ${currentResult})`
+
+outputResult(currentResult, calculationDescription);
+
