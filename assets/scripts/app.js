8a863858c9c7c9858a675c199ba956b1826a322d
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-15 18:34:39 +0100

    improved and cleaned code in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
 function add() {
-    const calcDescription = `${currentResult} + ${userInput.value}`;
-    currentResult = currentResult + parseInt(userInput.value);
+    const enteredNumber = getUserNumberInput()
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
     outputResult(currentResult, calcDescription);
 }
 
